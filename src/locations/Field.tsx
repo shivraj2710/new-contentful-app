ad6823c45bd5342d2b1978300f6c63d1ebfa876e
shivraj2710 <shivraj@compose.co.in>
2024-05-03 14:14:29 +0530

    event handler update

--- src/locations/Field.tsx
+++ src/locations/Field.tsx
@@ -1,31 +1,28 @@
 import { FieldAppSDK } from '@contentful/app-sdk';
-import { Paragraph,  } from '@contentful/f36-components';
-import {  useAutoResizer, useCMA,  useFieldValue,  useSDK } from '@contentful/react-apps-toolkit';
+import { useAutoResizer, useCMA, useFieldValue, useSDK } from '@contentful/react-apps-toolkit';
 import { useRef } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 
 
 const Field = () => {
   const sdk = useSDK<FieldAppSDK>();
-  const cma = useCMA();
-  const editorRef = useRef<any>(null); 
+  const editorRef = useRef<any>(null);
   const [value, setValue] = useFieldValue<string>(sdk.field.id, 'en-US');
   useAutoResizer();
 
   const log = () => {
     if (editorRef.current) {
-      console.log(editorRef.current, "SDTEST");
       setValue(editorRef.current.getContent());
     }
   };
 
-  return <>
-  <div style={{display: 'flex', flexDirection: 'column'}}>
-  <Editor
-        onKeyPress={()=>console.log('changed')}
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <Editor
+        onKeyPress={log}
         apiKey='ew3aymb0j6kompypnggrtkkk90goygpbj5fz3hz6cd57d5gy'
         onInit={(_evt, editor) => editorRef.current = editor}
-        initialValue="<p>This is the initial content of the editor.</p>"
+        initialValue={value ?? ''}
         init={{
           height: 500,
           menubar: true,
@@ -41,10 +38,7 @@ const Field = () => {
           content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
         }}
       />
-      <button onClick={log}>Log editor content</button>
-    </div>
-    <input value={value ?? ''} onChange={(e) => setValue(e.target.value)} />
-  <Paragraph>Hello Entry Field Component (AppId: {sdk.ids.app})</Paragraph></>;
+    </div>)
 };
 
 export default Field;
