587e0884179b66c7350b0d30e935d60507d3d28c
shivraj2710 <shivraj@compose.co.in>
2024-05-03 14:10:05 +0530

    added event handler updated

--- src/locations/Field.tsx
+++ src/locations/Field.tsx
@@ -22,7 +22,7 @@ const Field = () => {
   return <>
   <div style={{display: 'flex', flexDirection: 'column'}}>
   <Editor
-        onChange={()=>console.log('changed')}
+        onKeyPress={()=>console.log('changed')}
         apiKey='ew3aymb0j6kompypnggrtkkk90goygpbj5fz3hz6cd57d5gy'
         onInit={(_evt, editor) => editorRef.current = editor}
         initialValue="<p>This is the initial content of the editor.</p>"
